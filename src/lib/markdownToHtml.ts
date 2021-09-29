8ace00eaf396a265224e3831feb4539d53b60f4a
Felix Becker <felix.b@outlook.com>
2021-09-29 18:08:25 +0200

    Rewrite links to remove .md suffix and index.md

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,3 +1,5 @@
+import * as url from 'url'
+
 import rehypeExtractToc, { Toc } from '@stefanprobst/rehype-extract-toc'
 import { Root as HastRoot } from 'hast'
 import { select } from 'hast-util-select'
@@ -6,6 +8,7 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeRaw from 'rehype-raw'
 import rehypeSlug from 'rehype-slug'
 import rehypeStringify from 'rehype-stringify'
+import rehypeUrl, { UrlMatch } from 'rehype-url-inspector'
 import remarkGfm from 'remark-gfm'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
@@ -24,6 +27,11 @@ export default async function markdownToHtml(markdown: string): Promise<{ conten
         // Parse Markdown that was included _within_ HTML
         .use(rehypeRaw)
         .use(rehypeMarkupDates)
+        // Trim .md suffix from links
+        .use(rehypeUrl, {
+            selectors: ['a[href]'],
+            inspectEach: rewriteLinkUrl,
+        })
         // Add IDs to headings
         .use(rehypeSlug)
         // Extract title from H1 and attach as `vfile.data.title`
@@ -35,6 +43,30 @@ export default async function markdownToHtml(markdown: string): Promise<{ conten
         .process(markdown)
 
     return { content: result.toString(), title: result.data.title as string, toc: result.data.toc as Toc }
+}
+
+/**
+ * Rewrite links to `.md` files and to `index.md` files.
+ */
+function rewriteLinkUrl(match: UrlMatch): void {
+    // Use lenient URL parser since the URL can be relative. Make sure to preserve hash fragment.
+    const parsedUrl = url.parse(match.url)
+    if (parsedUrl.hostname) {
+        // Ignore absolute links
+        return
+    }
+    // Rewrite index.md references to point to the directory
+    if (parsedUrl.pathname?.match(/(^|\/)index\.md$/)) {
+        parsedUrl.pathname = url.resolve(parsedUrl.pathname, '.') || '.'
+    }
+    parsedUrl.pathname = parsedUrl.pathname
+        // Remove .md suffix
+        ?.replace(/\.md$/, '')
+        // Our routing currently redirects trailing slash to non-trailing slash anyway, so trim it
+        // earlier to save a redirect
+        .replace(/\/$/, '')
+
+    match.node.properties.href = url.format(parsedUrl)
 }
 
 /**
