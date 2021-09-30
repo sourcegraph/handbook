7af22fe3d614b6ba218d512ea5925e1d2bbb9627
Marek <marekz@gmail.com>
2021-09-30 14:33:54 -0400

    Fix lint and formatting

--- src/scripts/copy-assets.ts
+++ src/scripts/copy-assets.ts
@@ -1,23 +1,28 @@
-import path from 'path'
 import fs from 'fs/promises'
+import path from 'path'
+
 import globby from 'globby'
 import mkdirp from 'mkdirp'
+
 import { CONTENT_FOLDER } from '../lib/constants'
 
 const PUBLIC_FOLDER = 'public'
 
-export default async function copyAssets() {
+export default async function copyAssets(): Promise<void> {
     const targetFiles = globby.sync(['**/*.*', '!**/*.md'], { cwd: CONTENT_FOLDER })
     console.log(`Copying ${targetFiles.length} static files from ${CONTENT_FOLDER} to ${PUBLIC_FOLDER}`)
     for (const file of targetFiles) {
         const publicFolder = path.join(process.cwd(), PUBLIC_FOLDER)
         const sourceFolder = path.join(process.cwd(), CONTENT_FOLDER)
-        const dirName = path.dirname(file)
-        await mkdirp(path.join(publicFolder, dirName))
+        const directoryName = path.dirname(file)
+        await mkdirp(path.join(publicFolder, directoryName))
         const sourcePath = path.join(sourceFolder, file)
         const destinationPath = path.join(publicFolder, file)
         await fs.copyFile(sourcePath, destinationPath)
     }
 }
 
-copyAssets()
+copyAssets().catch(error => {
+    console.error(error)
+    process.exit(1)
+})
