eb41a9bba1e7f0364ef7475fe5bf37dafb2c138d
Jean du Plessis <jean@sourcegraph.com>
2021-09-30 21:22:44 +0200

    Move Swiftype JS to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,6 +20,7 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
                 strategy="lazyOnload"
                 src="https://www.googletagmanager.com/gtag/js?id=G-KVVCR189Q6"
             />
+
             <Script strategy="lazyOnload">
                 {`
                     window.dataLayer = window.dataLayer || [];
@@ -30,6 +31,18 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
                     });
                 `}
             </Script>
+
+            <Script strategy="lazyOnload">
+                {`
+                    (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+                    (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+                    e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
+                    })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+
+                    _st('install','JAPrEEBxHhYT4SnMJQmX','2.0.0');
+                `}
+            </Script>
+
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
             </Head>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -79,18 +79,6 @@ export default class MyDocument extends Document {
                             </div>
                         </footer>
                     </div>
-                    <script
-                        type="text/javascript"
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                                (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
-                                (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
-                                e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
-                                })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
-
-                                _st('install','JAPrEEBxHhYT4SnMJQmX','2.0.0');`,
-                        }}
-                    />
                 </body>
             </Html>
         )
