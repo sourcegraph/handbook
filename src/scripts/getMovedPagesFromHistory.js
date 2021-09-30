314e83d78fd0a054f521d088089f0a3e652a23da
Felix Becker <felix.b@outlook.com>
2021-10-01 01:42:47 +0200

    Fix redirects for moves from/to index file

--- src/scripts/getMovedPagesFromHistory.js
+++ src/scripts/getMovedPagesFromHistory.js
@@ -3,14 +3,21 @@ const exec = require('execa')
 async function getMovedPagesFromHistory() {
   const result = await exec('git', ['log', '--name-status', '--pretty=', '--diff-filter=R', 'content'])
 
-  const movedFilesFromHistory = result.stdout.split('\n').map(line => {
-    const [status, source, destination] = line.split('\t')
-    return {
-      status,
-      source: source.replace(/^content/, '').replace(/\.md$/, ''),
-      destination: destination.replace(/^content/, '').replace(/\.md$/, ''),
-    }
-  })
+  const movedFilesFromHistory = result.stdout
+    .split('\n')
+    .map(line => {
+      const [source, destination] = line
+        .split('\t')
+        .slice(1)
+        .map(path =>
+          path
+            .replace(/^content/, '')
+            .replace(/\.md$/, '')
+            .replace(/\/index/, '')
+        )
+      return { source, destination }
+    })
+    .filter(({ source, destination }) => destination !== source)
 
   return movedFilesFromHistory
 }
