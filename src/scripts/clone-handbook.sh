b651678646c1df805bbca674fe3fa8e4ea82fa8f
Marek <marekz@gmail.com>
2021-09-30 17:35:47 -0400

    Add comments to clone script

--- src/scripts/clone-handbook.sh
+++ src/scripts/clone-handbook.sh
@@ -1,3 +1,11 @@
+# This is a temporary script that replaces the `content` directory with a cloned
+# copy from the old handbook.
+
+# When it's time to remove this:
+# 1. Delete this script
+# 2. In package.json, remove `./src/scripts/clone-handbook.sh` from the
+#    "netlify-build" command.
+
 echo "Cloning the handbook from sourcegraph/about"
 rm -rf content/ about/
 git clone https://github.com/sourcegraph/about
