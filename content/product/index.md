c6e633b831898d1a447b8a18324dd31e4ed64df5
Jason Yavorska <jason@metaluna.io>
2021-10-22 16:05:16 +0300

    Add rendered maturity page

--- content/product/index.md
+++ content/product/index.md
@@ -15,6 +15,10 @@ You can reach us at the #product channel or @product-team on Slack. If you have 
 
 The product strategy and what we're working on next can be found on the [Sourcegraph strategy page](../company/strategy/index.md). There is also an (internal only) [roadmap presentation](https://docs.google.com/presentation/d/1o3R8WUIhzzRz0x5laTwVcizOzVWrMBe5MCAz74H45Ss/edit#slide=id.gf131fe1596_2_7) available which highlights major features coming next as well as what was recently shipped.
 
+## Feature Maturity
+
+We have an page listing our [features and maturity levels](maturity.md).
+
 ## Metrics
 
 - [Product metrics dashboard](https://sourcegraph.looker.com/dashboards/127)
---
+++ src/scripts/generated-pages.mjs
@@ -0,0 +1,63 @@
+import { load } from 'js-yaml';
+import { readFileSync, writeFile } from 'fs';
+import { exit } from 'process';
+
+function yaml_load(file) {
+    try {
+        return load(readFileSync(file, 'utf8'));
+    } catch (e) {
+        console.log(e);
+        exit();
+    }
+}
+
+function generate_maturity(features, maturity_levels, pricing_tiers, product_areas, product_orgs) {
+    const maturity_file = 'content/product/maturity.md';
+    var maturity_content = "# Product Features & Maturity\n";
+
+    Object.keys(product_areas).forEach((product_area) => {
+        maturity_content += "\n## " + product_areas[product_area].title + "\n"
+        maturity_content += "[" + product_orgs[product_areas[product_area].product_org].title + " Strategy Page](" + product_orgs[product_areas[product_area].product_org].strategy_link + ") | "
+        maturity_content += "[" + product_areas[product_area].title + " Strategy Page](" + product_areas[product_area].strategy_link + ")\n"
+        if (product_areas[product_area].description) {
+            maturity_content += "\n__" + product_areas[product_area].description + "__\n"
+        }
+        if (product_areas[product_area].limitations_link) {
+            maturity_content += "\n- Limitations: " + product_areas[product_area].limitations_link + "\n"
+        }
+
+        Object.keys(features).forEach((feature) => {
+            if (features[feature].product_area == product_area) {
+                maturity_content += "\n### " + features[feature].title + "\n"
+                if (features[feature].documentation_link) {
+                    maturity_content += "[Documentation](" + features[feature].documentation_link + ")\n"
+                }
+                if (features[feature].description) {
+                    maturity_content += "__" + features[feature].description + "__\n"
+                }
+                maturity_content += "\n- Maturity: " + maturity_levels[features[feature].maturity].title + "\n"
+                if (features[feature].available_in) {
+                    maturity_content += "- Available in: " + features[feature].available_in + "\n" //TODO render this properly
+                }
+                if (features[feature].limitations_link) {
+                    maturity_content += "- Limitations: " + features[feature].limitations_link + "\n" //TODO render as links
+                }
+                //TODO render compatibility matrix (maybe, or maybe that should go on another page)
+            }
+        });
+    });
+    writeFile(maturity_file, maturity_content, function (err) {
+        if (err) throw err;
+        console.log('Created ' + maturity_file);
+    });
+}
+
+// Load YAML files
+const features = yaml_load('data/features.yml');
+const maturity_levels = yaml_load('data/maturity_levels.yml');
+const pricing_tiers = yaml_load('data/pricing_tiers.yml');
+const product_areas = yaml_load('data/product_areas.yml');
+const product_orgs = yaml_load('data/product_orgs.yml');
+
+generate_maturity(features, maturity_levels, pricing_tiers, product_areas, product_orgs);
+
