9d38c91e4ed7222e67c81eb459811e99f9d098c7
Jason Yavorska <jason@metaluna.io>
2021-10-22 19:41:39 +0300

    Add features by tier page

--- src/scripts/generated-pages.mjs
+++ src/scripts/generated-pages.mjs
@@ -19,9 +19,12 @@ function make_maturity_link_relative(link) {
     }
 }
 
-function generate_maturity(features, maturity_levels, pricing_tiers, product_areas, product_orgs) {
+function generate_maturity_page(features, maturity_levels, pricing_tiers, product_areas, product_orgs) {
     const maturity_file = 'content/product/maturity.md';
     var maturity_content = "# Product Features & Maturity\n";
+    maturity_content += "This page shows the complete list of top-level features, organized by product area,\n"
+    maturity_content += "along with code host compatibility, any limitations, and the pricing tiers it is available in.\n"
+    maturity_content += "You may also be interested in seeing [features by pricing tier](features_by_tier.md).\n"
 
     Object.keys(product_areas).forEach((product_area) => {
         maturity_content += "\n## " + product_areas[product_area].title + "\n"
@@ -84,16 +87,20 @@ function make_tiers_link_relative(link) {
     }
 }
 
-function generate_tiers(features, maturity_levels, pricing_tiers, product_areas, product_orgs) {
+function generate_tiers_page(features, maturity_levels, pricing_tiers, product_areas, product_orgs) {
     const tiers_file = 'content/product/features_by_tier.md';
     var tiers_content = "# Product Features by Tier\n";
+    tiers_content += "This is a complete list of features by pricing tier. Each item will link you to our documentation,\n"
+    tiers_content += "and you can also see what level of maturity each feature is at.\n"
+    tiers_content += "You may also be interested in seeing [features by maturity](maturity.md).\n"
 
     Object.keys(pricing_tiers).forEach((pricing_tier) => {
         tiers_content += "\n## " + pricing_tiers[pricing_tier].title + "\n"
 
         Object.keys(features).forEach((feature) => {
             if (features[feature].available_in[pricing_tier]) {
-                tiers_content += "- " + features[feature].title + "\n"
+                tiers_content += "- [" + features[feature].title + "](" + make_tiers_link_relative(features[feature].documentation_link) + ")"
+                tiers_content += ": " + maturity_levels[features[feature].maturity].title + "\n"
             }
         });
     });
@@ -111,6 +118,6 @@ const product_areas = yaml_load('data/product_areas.yml');
 const product_orgs = yaml_load('data/product_orgs.yml');
 const code_hosts = yaml_load('data/code_hosts.yml');
 
-generate_maturity(features, maturity_levels, pricing_tiers, product_areas, product_orgs);
-generate_tiers(features, maturity_levels, pricing_tiers, product_areas, product_orgs);
+generate_maturity_page(features, maturity_levels, pricing_tiers, product_areas, product_orgs);
+generate_tiers_page(features, maturity_levels, pricing_tiers, product_areas, product_orgs);
 
