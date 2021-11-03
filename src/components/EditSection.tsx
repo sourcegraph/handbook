f3f04d27ffe78db1c991d10c14d03b1722c5574e
Malo Marrec <malo.marrec@gmail.com>
2021-11-03 11:51:58 +0100

    How to triage issues with unclear owner (#593)
    * add how to triage issues with unclear owner
    * add autogened product areas page
    
    Co-authored-by: Jason Yavorska <jason.yavorska@sourcegraph.com>
    Co-authored-by: malomarrec <malomarrec@users.noreply.github.com>

--- src/components/EditSection.tsx
+++ src/components/EditSection.tsx
@@ -57,6 +57,11 @@ export const EditSection: React.FunctionComponent<EditSectionProps> = ({ page })
                     Edit this data on GitHub
                 </Link>
             )}
+            {page.path === 'product/product_areas.md' && (
+                <Link href="https://github.com/sourcegraph/handbook/edit/main/data/product_areas.yml">
+                    Edit this data on GitHub
+                </Link>
+            )}
             {page.path === 'product/feature_maturity.md' && (
                 <Link href="https://github.com/sourcegraph/handbook/edit/main/data/features.yml">
                     Edit this data on GitHub
@@ -69,6 +74,7 @@ export const EditSection: React.FunctionComponent<EditSectionProps> = ({ page })
             )}
             {page.path !== 'company/team/index.md' &&
                 page.path !== 'product/feature_maturity.md' &&
+                page.path !== 'product/product_areas.md' &&
                 page.path !== 'product/feature_compatibility.md' && (
                     <Link href={`https://github.com/sourcegraph/handbook/edit/main/${CONTENT_FOLDER}/${page.path}`}>
                         Edit this page on GitHub
