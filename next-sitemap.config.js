b980d971804692736365bbebc65aad6377e0950c
bretthayes <bretthayes@users.noreply.github.com>
2022-08-04 15:07:08 +0000

    squash! Prettier

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://handbook.sourcegraph.com',
   generateRobotsTxt: true,
-  generateIndexSitemap: false
+  generateIndexSitemap: false,
 }
